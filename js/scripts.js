42638690a991c607097e3c046751e1e1e600bcb4
Joanna Fengler <asiapobuta@wp.pl>
2018-03-23 23:27:54 +0100

    practice DOM

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var buttons = document.getElementsByClassName('button');
+console.log(buttons);
+for (var i=0; i<=buttons.length; i++) {
+	//console.log(buttons[i]);
+	var buttons = document.getElementsByClassName('button');
+	console.log(buttons[i].innerText);
+}
+
+
+	
