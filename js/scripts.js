8ab0406ded79bb6d64fac6c57a3199d6739a2754
Joanna Fengler <asiapobuta@wp.pl>
2018-03-25 18:56:59 +0200

    correct loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,7 @@
 var buttons = document.getElementsByClassName('button');
-console.log(buttons);
-for (var i=0; i<=buttons.length; i++) {
-	//console.log(buttons[i]);
-	var buttons = document.getElementsByClassName('button');
+for (var i=0; i<buttons.length; i++) {
 	console.log(buttons[i].innerText);
-}
+};
 
 
 	
